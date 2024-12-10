6ce4571433a699fe34cb2a6261f9c42056424862
Alex Taranne <alextaranne@gmail.com>
2024-12-10 12:38:28 +0100

    STEP 2 JS FAIT

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,4 +3,18 @@ const img = document.querySelector(".avatar2");
 img.addEventListener("click", () => {
 	img.src = "image/avatar.svg";
 });
-console.log(img);
+
+const section = document.querySelector(".description");
+const button = document.createElement("button");
+button.textContent = "Change the color and text";
+section.appendChild(button);
+
+const fisrtName = document.querySelector("#firstname");
+
+button.addEventListener("click", () => {
+	const userName = prompt;
+	if (userName() === "alex") {
+		fisrtName.innerHTML = "alex";
+		document.querySelector("#firstname").style.color = "white";
+	}
+});
