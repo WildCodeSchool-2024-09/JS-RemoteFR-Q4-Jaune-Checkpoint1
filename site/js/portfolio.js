abe0da042b3d085aad9dbd7a0847c5adf6b03576
Killian0111 <45.killian.georges@gmail.com>
2024-12-10 12:06:25 +0100

    Step 2 JS

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -7,10 +7,25 @@ const avatars = [
 	},
 ];
 
+//step1
 const avatar = document.querySelector(".avatarjs");
+//step 2
+const button = document.getElementById("button");
+const firstNameElem = document.getElementById("firstname");
 
+// step 1
 avatar.addEventListener("click", () => {
 	const newImg =
 		"https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png";
 	avatar.src = newImg;
+});
+
+// step 2
+button.addEventListener("click", () => {
+	const userName = prompt("Enter your name :");
+
+	if (userName) {
+		firstNameElem.textContent = userName;
+		firstNameElem.style.color = "white";
+	}
 });
