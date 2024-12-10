91d16bfdffc17a36f117eeb2f8ccc89a9737958c
Duncan Hamelin <Duncan.hamelin@orange.fr>
2024-12-10 11:11:09 +0100

    JS Step 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,3 +3,13 @@ const avatar = document.getElementById("avatar");
 avatar.addEventListener("click", function() {
     avatar.src = "./image/avatar.svg"
 });
+
+
+// Step 2
+const modifyButton = document.getElementById("modify");
+modifyButton.addEventListener("click", function () {
+    const userName = prompt("Enter your name:");
+    const naming = document.getElementById("firstname");
+    naming.textContent = userName;
+    naming.style.color = "white";
+})
