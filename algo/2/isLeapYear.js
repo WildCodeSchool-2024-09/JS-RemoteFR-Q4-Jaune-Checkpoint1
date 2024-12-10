2bb0f118fdc4a9e99661d2f1e6c2ececd9fac8ec
Clarelle974 <clarelle.lemonnier@gmail.com>
2024-12-10 10:59:55 +0100

    STEP2 Portfolio : Css step1-6

--- algo/2/isLeapYear.js
+++ algo/2/isLeapYear.js
@@ -11,7 +11,8 @@ function isLeapYear(year) {
   if ((year%4 === 0 && year%100 !==0) || year%400===0){
     return true;
   }
-  else {return false}
+  else {
+    return false;}
 }
 
 module.exports = isLeapYear;
