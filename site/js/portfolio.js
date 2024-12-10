31a4a50a0087443a4840d072c1f83f955e91ca9d
Julien PICART <julienpicart01@gmail.com>
2024-12-10 12:32:44 +0100

    STEP 4 JS - FAIT

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -9,12 +9,19 @@ avatarImg.addEventListener("click", () => {
 const changeName = document.getElementById("firstname");
 const button = document.querySelector(".change-name");
 const changeBg = document.querySelector(".description");
+const footer = document.getElementById("footer");
+const changeTextColor = document.querySelectorAll(".pink-text");
 
 button.addEventListener("click", () => {
   const changeBgColor = prompt("Enter your color");
   const userName = prompt("What's your name ?");
-  changeBg.style.backgroundColor = changeBgColor;
-  button.style.backgroundColor = changeBgColor;
   changeName.textContent = userName;
   changeName.style.color = "white";
+  changeBg.style.backgroundColor = changeBgColor;
+  button.style.backgroundColor = changeBgColor;
+  footer.style.backgroundColor = changeBgColor;
+
+  for (const textColor of changeTextColor) {
+    textColor.style.color = changeBgColor;
+  }
 });
