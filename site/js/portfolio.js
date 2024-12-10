f66da0e5681e4243fd8ef7b3f87a6c84a8ab1bf7
Julien PICART <julienpicart01@gmail.com>
2024-12-10 11:36:03 +0100

    STEP 2 JS - FAIT

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,5 +1,16 @@
+// Changement de l'avatar
 const avatarImg = document.querySelector(".avatar-img");
 
 avatarImg.addEventListener("click", () => {
   avatarImg.src = "/site/image/avatar.svg";
+});
+
+// Changement du prénom
+const changeName = document.getElementById("firstname");
+const button = document.querySelector(".change-name");
+
+button.addEventListener("click", () => {
+  const userName = prompt("What's your name ?");
+  changeName.textContent = userName;
+  changeName.style.color = "white";
 });
