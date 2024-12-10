3e423704cd03ced9633caccc145e5515e19fbd34
MaxenceGitDev <maxence.pruvot@icloud.com>
2024-12-10 12:34:01 +0100

    JS Step4 title color ok / background footer pas ok

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -16,21 +16,49 @@ imgAvatar.addEventListener("click", () => {
 });
 
 //STEP 2
-const sectionButton = document.querySelector(".description");
-console.log(sectionButton);
+// const sectionButton = document.querySelector(".description");
 
+// const button = document.createElement("button");
+// button.textContent = "Modify";
+// sectionButton.appendChild(button);
+
+// const modifiedName = document.getElementById("firstname");
+
+// button.addEventListener("click", () => {
+//   const textUser = prompt("entrez votre prénom");
+//   // STEP 3
+//   const colorUSer = prompt("entrez une couleur # : ");
+//   modifiedName.textContent = textUser;
+//   modifiedName.style.color = "white";
+//   // STEP 3
+//   sectionButton.style.background = colorUSer;
+// });
+
+// STEP 4
+// cibles
+const sectionButton = document.querySelector(".description");
+const modifiedName = document.getElementById("firstname");
 const button = document.createElement("button");
 button.textContent = "Modify";
 sectionButton.appendChild(button);
 
-const modifiedName = document.getElementById("firstname");
+const pinkTitles = document.querySelectorAll(".pink-text");
 
+const pinkFooter = document.getElementsByTagName("footer");
+console.log(pinkFooter);
+
+// appliquer le style aux cibles
 button.addEventListener("click", () => {
   const textUser = prompt("entrez votre prénom");
-  // STEP 3
+
   const colorUSer = prompt("entrez une couleur # : ");
   modifiedName.textContent = textUser;
   modifiedName.style.color = "white";
-  // STEP 3
   sectionButton.style.background = colorUSer;
+  // titres
+  pinkTitles.forEach((title) => {
+    title.style.color = colorUSer;
+  });
+  // footer
+  pinkFooter.style.background = colorUSer;
 });
