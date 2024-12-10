3d662469f46836b9c07ce5ee4314203ae3777396
Lisa BELLALI <lisa.bellali92@gmail.com>
2024-12-10 13:19:54 +0100

    js

---
+++ site/js/portfolio.js
@@ -0,0 +1,8 @@
+document.getElementById("image").addEventListener("click", function() {
+    if(this.scroll.includes("avatar-2191931_1280.png")) {
+        this.src = "avatar-bis.png";
+    }
+    else {
+        this.src = "avatar-2191931_1280.png";
+    }
+});
