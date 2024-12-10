3fe87553179af61dc12e245a865470fba4311611
Clarelle974 <clarelle.lemonnier@gmail.com>
2024-12-10 13:24:04 +0100

    STEP2 JS : correction de la step1, ajout de la step optionnelle 5

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,18 +1,20 @@
-// consigne 1 : l'utilisateur peut changer d'avatar quand il clique dessus, je n'arrive pas à mettre toggle
+// consigne 1 : l'utilisateur peut changer d'avatar quand il clique dessus, ça passe
 const avatar = document.querySelector(".avatar");
-
 avatar.addEventListener("click", () => {
-//    if (avatar.src ==="image/avatar-bis.png"){
+    const isAv1 = avatar.src.endsWith("png");
+const isAv2 = avatar.src.endsWith("svg");
+   if (isAv1){
         avatar.src ="image/avatar.svg";
-    // }
-//    else {avatar.src ="image/avatar-bis.png"}
+    }
+   if (isAv2) {avatar.src ="image/avatar-bis.png"}
 });
 
-//consignes n°2-3-4 : modif du nom et de la couleur
+//consignes n°2-3-4-5 : modif du nom et de la couleur
 const modifyButton = document.querySelector(".modify");
 const firstname = document.getElementById("firstname");
 const coloredBackgrounds = document.querySelectorAll(".pink-bg");
 const coloredTexts = document.querySelectorAll(".pink-text");
+const navLinks = document.querySelectorAll("a");
 modifyButton.addEventListener("click", () => {
     const userColor = prompt('Enter your color', '#750ff7');
     for (const element of coloredBackgrounds){
@@ -21,9 +23,10 @@ modifyButton.addEventListener("click", () => {
     for (const text of coloredTexts){
         text.style.color = userColor;
     };
+    for (const link of navLinks){
+        link.style.color = userColor;
+    };
     const username = prompt('Enter your name');
     firstname.textContent=username;
     firstname.style.color = "white";
 })
-
-
