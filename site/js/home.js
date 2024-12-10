4c7a276e56bd262c5234fbdcc0a51c388dcd7bcb
JULIAN HAZARD <hazardjulian74@gmail.com>
2024-12-10 12:26:45 +0100

    JS STEP 1

--- site/js/home.js
+++ site/js/home.js
@@ -1,7 +1,9 @@
 function handleHelp(item) {
   const help = document.getElementsByClassName("help");
   const problem = document.getElementsByClassName("problem");
+  
 
   help[item].classList.toggle("help-visible");
   problem[item].classList.toggle("problem-no-visible");
+   
 }
---
+++ site/js/portfolio.js
@@ -0,0 +1,14 @@
+const imageAvatar = document.querySelector('.avatar');
+
+
+
+imageAvatar.addEventListener('click', function () {
+    imageAvatar.src = "image/avatar.svg";
+    imageAvatar.alt = "nouvel avatar";
+});
+
+
+
+
+  
+
