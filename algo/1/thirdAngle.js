85520047ba09be0c8c755f22740a0338999dc233
Max00T <trousselmaxime@gmail.com>
2024-12-10 10:09:33 +0100

    algo 1 & 2

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -7,7 +7,10 @@ thirdAngle(20, 80) doit retourner 80
 */
 
 function thirdAngle(a, b) {
-  // Your code here !
+	// Your code here !
+	const angle = 180 - (a + b);
+	return angle;
 }
+thirdAngle(90, 30);
 
 module.exports = thirdAngle;
