af1ceda6f80b8c760c8938319c5b07b2d6035660
Ochoooooo <greenfieldrp7@gmail.com>
2024-12-10 13:28:14 +0100

    JS 2 fait

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -2,8 +2,20 @@
 const avatar = document.querySelector('.avatar');
 
 avatar.addEventListener('click',() => {
-
 avatar.src = 'image/avatar.svg';
 avatar.alt = 'un avatar qui sourie';
+});
 
+const inser = document.querySelector('.pink-bg');
+const button = document.createElement('button');
+button.textContent = 'User'
+
+
+inser.appendChild(button);
+button.addEventListener('click',() =>{
+let name = prompt("Veuillez entrer votre Nom:");
+if ( name != "" || name != null){
+    const firstName = document.querySelector("#firstname");
+    firstName.textContent = name;
+}
 });
