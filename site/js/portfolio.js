9fb6aba72bcf7c5dca370f48c2e0dbb12c6b0912
Julien PICART <julienpicart01@gmail.com>
2024-12-10 11:59:15 +0100

    STEP 3 JS - FAIT

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -5,12 +5,16 @@ avatarImg.addEventListener("click", () => {
   avatarImg.src = "/site/image/avatar.svg";
 });
 
-// Changement du prénom
+// Changement du prénom et couleur du background
 const changeName = document.getElementById("firstname");
 const button = document.querySelector(".change-name");
+const changeBg = document.querySelector(".description");
 
 button.addEventListener("click", () => {
+  const changeBgColor = prompt("Enter your color");
   const userName = prompt("What's your name ?");
+  changeBg.style.backgroundColor = changeBgColor;
+  button.style.backgroundColor = changeBgColor;
   changeName.textContent = userName;
   changeName.style.color = "white";
 });
